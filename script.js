ac6f34e1d619d3d8c76567d89dc80f76708a2e30
Neha Mer <125866423+Neha-mer-13@users.noreply.github.com>
2024-08-11 13:25:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,41 +35,20 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["UI UX Designer"],
+        strings: ["UI UX Designer, Graphics Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["UI UX Designer"],
+        strings: ["UI UX Designer , Graphics Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-//     // owl carousel script
-//     $('.carousel').owlCarousel({
-//         margin: 20,
-//         loop: true,
-//         autoplay: true,
-//         autoplayTimeOut: 2000,
-//         autoplayHoverPause: true,
-//         responsive: {
-//             0:{
-//                 items: 1,
-//                 nav: false
-//             },
-//             600:{
-//                 items: 2,
-//                 nav: false
-//             },
-//             1000:{
-//                 items: 3,
-//                 nav: false
-//             }
-//         }
-//     });
+
  });
 
 // ----------------- contact -------------
@@ -85,7 +64,7 @@ form.addEventListener('submit', e => {
             body: new FormData(form)
         })
         .then(response => {
-            msg.innerHTML = "Massage sent successfully"
+            msg.innerHTML = "Message sent successfully"
             setTimeout(function() {
                 msg.innerHTML = ""
             }, 5000)
